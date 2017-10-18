cf63866050c3a24d5f5371e6a83af87943796b9d
Kevin@test.com <kevin@test.com>
2017-10-18 11:42:36 +0100

    cookie and guess comparrison working

--- go-guessing-game.go
+++ go-guessing-game.go
@@ -15,6 +15,8 @@
 *https://github.com/gowww/client/blob/master/response.go
 *https://godoc.org/hkjn.me/googleauth
 *https://golang.org/pkg/strconv/
+*https://stackoverflow.com/questions/26189523/go-represent-path-without-query-string
+*https://stackoverflow.com/questions/20320549/how-can-you-delete-a-cookie-in-an-http-response
 */
 
 package main
@@ -32,12 +34,14 @@ type TodoPageData struct {
 	//name of template variables to be used in .tmpl file
 	PageTitle string
 	GuessTmpl string
+	ResultTmpl string
+	CorrectTmpl string
 }
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	// guessing game echoed out
 	//fmt.Fprintln(w, "<h1>Guessing Game</h1>")
 
-////////############################Cookie start#################################
+	
 	//condition to check if the cookie length is 0 (exists)
 	//it will run this only once if it is 0 as the next time it 
 	//will have avlue greater than  0.
@@ -59,36 +63,80 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 
 	// write the cookie to response
 	http.SetCookie(w, &ck)
-}
-	// read cookie
-	var cookie,err = r.Cookie("target")
-	if err == nil {
-		//test the value target on the terminal
-		var cookievalue = cookie.Value
-		fmt.Println(w, "<b>get cookie value is " + cookievalue + "</b>\n")
 	}
+
+
+	
+	//Initialise string for use outside of conditional
+	 guessStr := ""
+	 tmplGuess := ""
+	if len(r.URL.Query().Get("guess")) >= 0{
 	//@ToDo form validation for integer value
 	//create variable guessStr and store the users guess from the client side
-	 guessStr := r.URL.Query().Get("guess")
+	guessStr = r.URL.Query().Get("guess")
+	tmplGuess = string(guessStr)
+	}
 	//Cast to string
-	 tmplGuess := string(guessStr)
-
+	
+	
+	
+	
 	//test guess var in terminal when the submit button is pressed
 	//fmt.Println(guess)
-//////////########################Cookie end###############################
+	//for result message template
+	var result string
+	var correct string
+	// read cookie
+	var cookie,err = r.Cookie("target")
+	//if cookie is set
+	if err == nil {
+		//set strings tmplGuess and cookie.Value to int
+		guessInt,_ := strconv.Atoi(tmplGuess)
+		tarInt,_ := strconv.Atoi(cookie.Value)
+		//conditional to check for parity between the two numbers
+		if guessInt < tarInt{
+			result = "you have guessed too low!!!"
+		}else if guessInt > tarInt{
+			result = "You have guessed too high!!!"
+		}else{//reset the cookie length to 0 if the number has been guessed
+			correct = "Well done you have guessed the correct number!!!!!!!"
+			expires := time.Now().AddDate(1, 0, 0)
+			ck := http.Cookie{
+				//set target for the random number
+				Name: "target",
+				Path: "/",
+				Expires: expires,
+			}
+			//one single random value
+			rand.Seed(time.Now().UnixNano())
+			tarNum := rand.Intn(20-1) + 1
+			// value of cookie converted to string
+			//@todo convert back to integer when comparing the values
+			ck.Value = strconv.Itoa(tarNum)
+		
+			// write the cookie to response
+			http.SetCookie(w, &ck)
+		}
+		//test the value target on the terminal
+		//struct set string for the template
+	data := TodoPageData{
+	PageTitle: "Pick a number between 1 and 20",
+	GuessTmpl: tmplGuess,
+	ResultTmpl: result,
+	CorrectTmpl: correct,
+	}
+	//parse the static folder for any template files
+	tmpl := template.Must(template.ParseGlob("static/*"))
+	//pass PageTile string to guess.tmpl
+	tmpl.ExecuteTemplate(w,"guess.tmpl", data)	
+		fmt.Println(w, "<b>get cookie value is " , guessInt , tarInt , "</b>\n")
+	}
+	
 
 	//set the header content type to text/html
 	w.Header().Set("Content-Type", "text/html")
 	
-   //struct set string for the template
-	data := TodoPageData{
-		PageTitle: "Pick a number between 1 and 20",
-		GuessTmpl: tmplGuess,
-		}
-	//parse the static folder for any template files
-	tmpl := template.Must(template.ParseGlob("static/*"))
-	//pass PageTile string to guess.tmpl
-	tmpl.ExecuteTemplate(w,"guess.tmpl", data)
+   
 	
 }
 

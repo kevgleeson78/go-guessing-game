6ebb70997b2f7aa8c30b75108d973f7e0748f8da
Kevin@test.com <kevin@test.com>
2017-10-23 14:46:59 +0100

    go fmt and template moved fro inside of cookie condition

--- go-guessing-game.go
+++ go-guessing-game.go
@@ -3,7 +3,7 @@
 *@Author:  Kevin Gleeson
 *Date:     15/10/2017
 *Version:  1.0
-*Sources: 
+*Sources:
 *https://github.com/data-representation/go-echo
 *https://golang.org/pkg/net/http/#SetCookie
 *https://stackoverflow.com/questions/12130582/setting-cookies-in-golang-net-http
@@ -17,97 +17,93 @@
 *https://golang.org/pkg/strconv/
 *https://stackoverflow.com/questions/26189523/go-represent-path-without-query-string
 *https://stackoverflow.com/questions/20320549/how-can-you-delete-a-cookie-in-an-http-response
-*/
+ */
 
 package main
 
 import (
-	"html/template"
-	"net/http"
-	"time"
 	"fmt"
+	"html/template"
 	"math/rand"
+	"net/http"
 	"strconv"
+	"time"
 )
+
 //struct declaration
 type TodoPageData struct {
 	//name of template variables to be used in .tmpl file
-	PageTitle string
-	GuessTmpl string
-	ResultTmpl string
+	PageTitle   string
+	GuessTmpl   string
+	ResultTmpl  string
 	CorrectTmpl string
 }
+
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	// guessing game echoed out
 	//fmt.Fprintln(w, "<h1>Guessing Game</h1>")
 
-	
 	//condition to check if the cookie length is 0 (exists)
-	//it will run this only once if it is 0 as the next time it 
+	//it will run this only once if it is 0 as the next time it
 	//will have avlue greater than  0.
-	
-	if len(r.Cookies()) == 0{
-	// cookie will expire after 1 year 
-    expires := time.Now().AddDate(1, 0, 0)
-	ck := http.Cookie{
-		//set target for the random number
-		Name: "target",
-		Path: "/",
-		Expires: expires,
-	}
-	//one single random value
-	rand.Seed(time.Now().UnixNano())
-	tarNum := rand.Intn(20-1) + 1
-	// value of cookie converted to string
-	//@todo convert back to integer when comparing the values
-	ck.Value = strconv.Itoa(tarNum)
 
-	// write the cookie to response
-	http.SetCookie(w, &ck)
+	if len(r.Cookies()) == 0 {
+		// cookie will expire after 1 year
+		expires := time.Now().AddDate(1, 0, 0)
+		ck := http.Cookie{
+			//set target for the random number
+			Name:    "target",
+			Path:    "/",
+			Expires: expires,
+		}
+		//one single random value
+		rand.Seed(time.Now().UnixNano())
+		tarNum := rand.Intn(20-1) + 1
+		// value of cookie converted to string
+		//@todo convert back to integer when comparing the values
+		ck.Value = strconv.Itoa(tarNum)
+
+		// write the cookie to response
+		http.SetCookie(w, &ck)
 	}
 
-    
-	
 	//Initialise string for use outside of conditional
-	 guessStr := ""
-	 tmplGuess := ""
-	 //@toDO check page not loading after initial guess.
-	if len(r.URL.Query().Get("guess")) >= 0{
-	r.ParseForm()
-	//@ToDo form validation for integer value
-	//create variable guessStr and store the users guess from the client side
-	guessStr = r.Form.Get("guess")
-	tmplGuess = string(guessStr)
+	guessStr := ""
+	tmplGuess := ""
+	//@toDO check page not loading after initial guess.
+	if len(r.URL.Query().Get("guess")) >= 0 {
+		r.ParseForm()
+		//@ToDo form validation for integer value
+		//create variable guessStr and store the users guess from the client side
+		guessStr = r.Form.Get("guess")
+		tmplGuess = string(guessStr)
 	}
 	//Cast to string
-	
-	
-	
-	
+
 	//test guess var in terminal when the submit button is pressed
 	//fmt.Println(guess)
 	//for result message template
 	var result string
 	var correct string
 	// read cookie
-	var cookie,err = r.Cookie("target")
+	var cookie, err = r.Cookie("target")
 	//if cookie is set
 	if err == nil {
 		//set strings tmplGuess and cookie.Value to int
-		guessInt,_ := strconv.Atoi(tmplGuess)
-		tarInt,_ := strconv.Atoi(cookie.Value)
+		guessInt, _ := strconv.Atoi(tmplGuess)
+		tarInt, _ := strconv.Atoi(cookie.Value)
 		//conditional to check for parity between the two numbers
-		if guessInt < tarInt{
+		if guessInt < tarInt {
 			result = "you have guessed too low!!!"
-		}else if guessInt > tarInt{
+		} else if guessInt > tarInt {
 			result = "You have guessed too high!!!"
-		}else{//reset the cookie length to 0 if the number has been guessed
+		} else { //reset the cookie length to 0 if the number has been guessed
 			correct = "Well done you have guessed the correct number!!!!!!!"
 			expires := time.Now().AddDate(1, 0, 0)
 			ck := http.Cookie{
 				//set target for the random number
-				Name: "target",
-				Path: "/",
+				Name:    "target",
+				Path:    "/",
 				Expires: expires,
 			}
 			//one single random value
@@ -116,37 +112,33 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 			// value of cookie converted to string
 			//@todo convert back to integer when comparing the values
 			ck.Value = strconv.Itoa(tarNum)
-		
+
 			// write the cookie to response
 			http.SetCookie(w, &ck)
 		}
-		//test the value target on the terminal
-		//struct set string for the template
+		fmt.Println(w, "<b>get cookie value is ", guessInt, tarInt, "</b>\n")
+	}
+	//test the value target on the terminal
+	//struct set string for the template
+	//@toDo place conditionals for dynamic result templates 
 	data := TodoPageData{
-	PageTitle: "Pick a number between 1 and 20",
-	GuessTmpl: tmplGuess,
-	ResultTmpl: result,
-	CorrectTmpl: correct,
+		PageTitle:   "Pick a number between 1 and 20",
+		GuessTmpl:   tmplGuess,
+		ResultTmpl:  result,
+		CorrectTmpl: correct,
 	}
 	//parse the static folder for any template files
 	tmpl := template.Must(template.ParseGlob("static/*"))
 	//pass PageTile string to guess.tmpl
-	tmpl.ExecuteTemplate(w,"guess.tmpl", data)	
-		fmt.Println(w, "<b>get cookie value is " , guessInt , tarInt , "</b>\n")
-	}
-	
+	tmpl.ExecuteTemplate(w, "guess.tmpl", data)
 
 	//set the header content type to text/html
 	w.Header().Set("Content-Type", "text/html")
-	
-   
-	
+
 }
 
-
 func main() {
-	
-	
+
 	//store the directory where the html and template files are held
 	fs := http.FileServer(http.Dir("static"))
 	//Start at the root directory
@@ -160,5 +152,5 @@ func main() {
 
 	//Listen out for requests to the server
 	http.ListenAndServe(":8080", nil)
-	
+
 }

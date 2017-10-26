4bd996753e8688063b07fdb3f0083bdf393ea535
Kevin@test.com <kevin@test.com>
2017-10-26 15:39:29 +0100

    guess.html removed, basic logic added to template file fro different displays of data

--- go-guessing-game.go
+++ go-guessing-game.go
@@ -19,14 +19,9 @@
 *https://stackoverflow.com/questions/20320549/how-can-you-delete-a-cookie-in-an-http-response
  */
 
- 
-//@toDo place conditionals for dynamic result templates
-//@toDO form input validation 
 package main
 
 import (
-	//only used fmt for debugging
-	//"fmt"
 	"html/template"
 	"math/rand"
 	"net/http"
@@ -37,21 +32,17 @@ import (
 //struct declaration
 type TodoPageData struct {
 	//name of template variables to be used in .tmpl file
-	PageTitle   string
+	Message     string
 	GuessTmpl   string
 	ResultTmpl  string
 	CorrectTmpl string
 }
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
-	// guessing game echoed out
-	//fmt.Fprintln(w, "<h1>Guessing Game</h1>")
-
-	//condition to check if the cookie length is 0 (exists)
-	//it will run this only once if it is 0 as the next time it
-	//will have avlue greater than  0.
-
-	if len(r.Cookies()) == 0 {
+	// read cookie
+	var cookie, err = r.Cookie("target")
+	//condition to check if the cookie is not set
+	if err != nil {
 		// cookie will expire after 1 year
 		expires := time.Now().AddDate(1, 0, 0)
 		ck := http.Cookie{
@@ -64,9 +55,7 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 		rand.Seed(time.Now().UnixNano())
 		tarNum := rand.Intn(20-1) + 1
 		// value of cookie converted to string
-		
 		ck.Value = strconv.Itoa(tarNum)
-
 		// write the cookie to response
 		http.SetCookie(w, &ck)
 	}
@@ -74,32 +63,26 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	//Initialise string for use outside of conditional
 	guessStr := ""
 	tmplGuess := ""
-	//condition to test if the guess variable is set
-	if len(r.Form.Get("guess")) >= 0 {
-		r.ParseForm()
-		//@ToDo form validation for integer value
-		//create variable guessStr and store the users guess from the client side
-		guessStr = r.Form.Get("guess")
-		tmplGuess = string(guessStr)
-	}
+
+	//method for getting the post ,ethod from the form text field
+	r.ParseForm()
+	//create variable guessStr and store the users guess from the client side
+	guessStr = r.Form.Get("guess")
 	//Cast to string
-
-	//test guess var in terminal when the submit button is pressed
-	//fmt.Println(guess)
-
-
-	//for result message template
+	tmplGuess = string(guessStr)
+	//result for the  ResultTmpl
 	var result string
+
+
+	//correct for CorrectTmpl
 	var correct string
-	// read cookie
-	var cookie, err = r.Cookie("target")
 	//if cookie is set
 	if err == nil {
 		//set strings tmplGuess and cookie.Value to int
 		guessInt, _ := strconv.Atoi(tmplGuess)
 		tarInt, _ := strconv.Atoi(cookie.Value)
 		//input validation for numbers between 1 and 20 only.
-		if guessInt > 0 && guessInt < 21{ 
+		if guessInt > 0 && guessInt < 21 {
 			//conditionals to check for parity between the two numbers
 			if guessInt < tarInt {
 				result = "you have guessed too low!!!"
@@ -107,7 +90,7 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 				result = "You have guessed too high!!!"
 			} else { //reset the cookie length to 0 if the number has been guessed
 				correct = "Well done you have guessed the correct number!!!!!!!"
-				//delete cookie
+				//delete cookie and reset a new one with a random value int
 				expires := time.Now().AddDate(1, 0, 0)
 				ck := http.Cookie{
 					//set target for the random number
@@ -115,27 +98,22 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 					Path:    "/",
 					Expires: expires,
 				}
-				
 				//one single random value
 				rand.Seed(time.Now().UnixNano())
 				tarNum := rand.Intn(20-1) + 1
 				// value of cookie converted to string
-				
 				ck.Value = strconv.Itoa(tarNum)
-
 				// write the cookie to response
 				http.SetCookie(w, &ck)
 			}
-		}else{
+		} else {
 			result = "That is not a valid number"
 		}
-			//Testing to console ouput 
-			//fmt.Println(w, "<b>get cookie value is ", guessInt, tarInt, "</b>\n")
 	}
-	
+
 	//struct set string for the template
 	data := TodoPageData{
-		PageTitle:   "Pick a number between 1 and 20",
+		Message:     "Pick a number between 1 and 20",
 		GuessTmpl:   tmplGuess,
 		ResultTmpl:  result,
 		CorrectTmpl: correct,
@@ -144,14 +122,11 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseGlob("static/*"))
 	//pass PageTile string to guess.tmpl
 	tmpl.ExecuteTemplate(w, "guess.tmpl", data)
-
 	//set the header content type to text/html
 	w.Header().Set("Content-Type", "text/html")
-
 }
 
 func main() {
-
 	//store the directory where the html and template files are held
 	fs := http.FileServer(http.Dir("static"))
 	//Start at the root directory
@@ -162,8 +137,6 @@ func main() {
 	http.Handle("./", fs)
 	//handle the guess request
 	http.HandleFunc("/guess", requestHandler)
-
 	//Listen out for requests to the server
 	http.ListenAndServe(":8080", nil)
-
 }
